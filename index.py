c3c784394438b27e2be6e35a0f65047c8b2ed75a
Lorenzo Leoni <lorenzo.leoni@vem.com>
2023-03-09 11:23:19 +0100

    add initial project structure

---
+++ index.py
@@ -0,0 +1,27 @@
+# navigation file
+import dash_core_components as dcc
+import dash_html_components as html
+import dash
+
+from app import app
+from app import server
+from page_layouts import overview_page, runScript_page
+import callbacks
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content')
+])
+
+@app.callback(dash.dependencies.Output('page-content', 'children'),
+              [dash.dependencies.Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/apps/overview':
+         return overview_page
+    elif pathname == '/apps/run_script':
+         return runScript_page
+    else:
+        return overview_page # This is the "home page"
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ page_layouts.py
@@ -0,0 +1,38 @@
+## all the pages layout
+
+
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import pandas as pd
+import numpy as np
+import dash
+import dash_table
+
+###################################################
+# DEFINE REUSABLE COMPONENTS AS FUNCTION
+###################################################
+
+#function
+
+
+###################################################
+# OVERVIEW PAGE LAYOUT
+###################################################
+
+overview_page = html.Div()
+
+
+###################################################
+# RUN PAGE LAYOUT
+###################################################
+
+
+## SSH part
+ssh_div = html.Div()
+
+## SNMP part
+snmp_div = html.Div()
+
+
+runScript_page = html.Div()
